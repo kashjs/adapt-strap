2e810df20f4c803e31c7b34b3bf50983978b9c52
Kashyap Patel <kashyap@adap.tv>
2014-07-25 13:37:08 -0700

    Validation errors fixed
    + jshint errors fixed
    + jscs styleguide errors fixed
    + docs added to gulp validation paths

--- docs/app.js
+++ docs/app.js
@@ -1,3 +1,4 @@
+// ========== modules documentation configuration ========== //
 window.adaptStrapModules = [
   {
     moduleName: 'treebrowser',
@@ -121,81 +122,85 @@ window.adaptStrapModules = [
   }
 ];
 
+
+// ========== initialize documentation app module ========== //
 angular.module('adaptv.adaptStrapDocs', [
   'ngSanitize',
   'adaptv.adaptStrap'
 ])
 
-// Render markdown in the HTML page
-angular.module('adaptv.adaptStrapDocs')
-  .constant('adaptStrapModules', adaptStrapModules)
-  .controller('layoutCtrl', ['$scope', '$anchorScroll', '$location', 'adaptStrapModules',
-    function ($scope, $anchorScroll, $location, adaptStrapModules) {
-      $scope.modules = adaptStrapModules;
-      $scope.scrollTo = function (id, $event) {
-        $event.preventDefault();
-        $location.hash(id);
-        $anchorScroll();
-      }
-    }
-  ])
-  .directive('markdown', function ($compile, $http) {
-    var converter = new Showdown.converter();
-    return {
-      restrict: 'E',
-      replace: true,
-      link: function (scope, element, attrs) {
-        function load() {
-          if ('src' in attrs) {
-            $http.get(attrs.src).then(function (data) {
-              var format = attrs.src.split('.');
-              if (format[format.length - 1] === 'js' || format[format.length - 1] === 'html') {
-                data.data = '```\n' + data.data + '\n```';
-              }
-              element.html(converter.makeHtml(data.data));
-              element.find('pre code').each(function (i, block) {
-                hljs.highlightBlock(block);
-              });
-            });
-          } else {
-            element.html(converter.makeHtml(element.html()));
+// ========== documentation support onstants =========== //
+.constant('adaptStrapModules', window.adaptStrapModules)
+
+// ========== documentation support controllers ========== //
+.controller('layoutCtrl', ['$scope', '$anchorScroll', '$location', 'adaptStrapModules',
+  function ($scope, $anchorScroll, $location, adaptStrapModules) {
+    $scope.modules = adaptStrapModules;
+    $scope.scrollTo = function (id, $event) {
+      $event.preventDefault();
+      $location.hash(id);
+      $anchorScroll();
+    };
+  }
+])
+
+// ========== documentation support directives ========== //
+.directive('markdown', function ($compile, $http) {
+  var converter = new Showdown.converter();
+  return {
+    restrict: 'E',
+    replace: true,
+    link: function (scope, element, attrs) {
+      function load() {
+        if ('src' in attrs) {
+          $http.get(attrs.src).then(function (data) {
+            var format = attrs.src.split('.');
+            if (format[format.length - 1] === 'js' || format[format.length - 1] === 'html') {
+              data.data = '```\n' + data.data + '\n```';
+            }
+            element.html(converter.makeHtml(data.data));
             element.find('pre code').each(function (i, block) {
               hljs.highlightBlock(block);
             });
-          }
+          });
+        } else {
+          element.html(converter.makeHtml(element.html()));
+          element.find('pre code').each(function (i, block) {
+            hljs.highlightBlock(block);
+          });
         }
-
-        attrs.$observe('src', function () {
-          load();
-        })
       }
-    };
-  })
-  .directive('codeNavigator', [function () {
-    return {
-      link: function (scope) {
-        scope.currentFile = scope.files[0];
-        scope.setCurrentFile = function (file) {
-          scope.currentFile = file;
-        }
-      },
-      restrict: 'E',
-      scope: {
-        files: '=',
-        basePath: '@'
-      },
-      templateUrl: 'docs/codeNavigator.html'
+      attrs.$observe('src', function () {
+        load();
+      });
     }
-  }])
-  .directive('componentOptions', [function () {
-    return {
-      link: function (scope) {
+  };
+})
+.directive('codeNavigator', [function () {
+  return {
+    link: function (scope) {
+      scope.currentFile = scope.files[0];
+      scope.setCurrentFile = function (file) {
+        scope.currentFile = file;
+      };
+    },
+    restrict: 'E',
+    scope: {
+      files: '=',
+      basePath: '@'
+    },
+    templateUrl: 'docs/codeNavigator.html'
+  };
+}])
+.directive('componentOptions', [function () {
+  return {
+    link: function (scope) {
 
-      },
-      restrict: 'E',
-      scope: {
-        module: '='
-      },
-      templateUrl: 'docs/componentOptions.html'
-    }
-  }])
+    },
+    restrict: 'E',
+    scope: {
+      module: '='
+    },
+    templateUrl: 'docs/componentOptions.html'
+  };
+}]);

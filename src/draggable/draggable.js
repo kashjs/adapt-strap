4f3df0fae59f3ad6fd98312cdcf602eb8d8035d8
Evangelia Batsi <evangelia.batsi@teamaol.com>
2015-06-16 14:10:42 +0100

    on drop remove reduced opacity  from the actual element

--- src/draggable/draggable.js
+++ src/draggable/draggable.js
@@ -229,7 +229,7 @@ angular.module('adaptv.adaptStrap.draggable', [])
           callback: onDragComplete
         });
         if (scope.useClonedElement) {
-          elem.removeClass('ad-dragging');
+          element.removeClass('ad-dragging');
           elem.remove();
         } else {
           elem.removeClass('ad-dragging');

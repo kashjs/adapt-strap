d4ff7c35c7fd258fb707e7403f42e89a8547e619
Slawomir Demichowicz <slawomir.demichowicz@teamaol.com>
2016-01-14 14:47:36 +0000

    v2.4.11

--- dist/adapt-strap.css
+++ dist/adapt-strap.css
@@ -1,6 +1,6 @@
 /**
  * adapt-strap
- * @version v2.4.10 - 2016-01-12
+ * @version v2.4.11 - 2016-01-14
  * @link https://github.com/Adaptv/adapt-strap
  * @author Kashyap Patel (kashyap@adap.tv)
  * @license MIT License, http://www.opensource.org/licenses/MIT
